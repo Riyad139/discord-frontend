494c54a7e80f3ab3ba66955e39291912ae0a64ad
Riyad139 <riyad.ahmed1886@gmail.com>
2023-03-19 03:07:08 +0600

    dash board created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,23 @@
+import SideBar from "@/Components/SideBar";
+import SeceondarySideBar from "@/Components/SideBar/SecondarySideBar";
+import { BsThreeDotsVertical } from "react-icons/bs";
 export default function Home() {
   return (
-    <div>
-      <p className="text-red-900">Hello</p>
+    <div className=" flex h-[100vh]">
+      <div className="h-full w-20 ">
+        <SideBar />
+      </div>
+      <div>
+        <SeceondarySideBar />
+      </div>
+      <div className="w-full h-full">
+        <div className="w-full flex flex-row-reverse items-center px-5 bg-lightGray h-16 border-b-2 border-dark">
+          <BsThreeDotsVertical cursor={"pointer"} color="white" size={20} />
+        </div>
+        <div className="w-full h-[93.2%] text-gray-200 flex justify-center items-center bg-lightGray">
+          <p>To start chating - selecet a converstion</p>
+        </div>
+      </div>
     </div>
   );
 }
