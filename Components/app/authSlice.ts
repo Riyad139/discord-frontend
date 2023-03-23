36c87ce65fd0f30d6d16b0614bd767dde5c905d6
Riyad139 <riyad.ahmed1886@gmail.com>
2023-03-23 09:02:38 +0600

    auth slice and user type is updated

--- Components/app/authSlice.ts
+++ Components/app/authSlice.ts
@@ -1,9 +1,14 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
+
 import api from "../Library/apiClient";
 
 export const signIn = createAsyncThunk("auth/user", async (data: any) => {
   const res = await api.post("/user/login", data);
+  return res.data;
+});
+
+export const signinUsingToken = createAsyncThunk("auth/reuser", async () => {
+  const res = await api.get("user/login");
   return res.data;
 });
 
@@ -23,6 +28,17 @@ const AuthSlice = createSlice({
       state.user = action.payload;
     });
     builder.addCase(signIn.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error.message || "";
+    });
+    builder.addCase(signinUsingToken.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(signinUsingToken.fulfilled, (state, action) => {
+      state.loading = false;
+      state.user = action.payload;
+    });
+    builder.addCase(signinUsingToken.rejected, (state, action) => {
       state.loading = false;
       state.error = action.error.message || "";
     });
