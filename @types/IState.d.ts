e1535009af91cb825035db3c1ad06209a8661504
Riyad139 <riyad.ahmed1886@gmail.com>
2023-03-24 05:48:27 +0600

    IState type added friend

--- @types/IState.d.ts
+++ @types/IState.d.ts
@@ -3,4 +3,5 @@ import { IUser } from "./IUser";
 
 export interface IState {
   Auth: { user: IUser; loading: boolean; error: undefined | string };
+  friend: { friend: string[]; invitation: string[]; onlineFriend: string[] };
 }
