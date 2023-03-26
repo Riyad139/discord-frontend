66a65491435509b9ee859965f00f19ebb02030f9
Riyad139 <riyad.ahmed1886@gmail.com>
2023-03-26 13:24:24 +0600

    chat slice is added to store

--- Components/app/store.ts
+++ Components/app/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import thunkMiddleware from "redux-thunk";
 import AuthReducer from "./authSlice";
 import friendReducer from "./friendSlice";
+import chatReducer from "./chatSlice";
 const store = configureStore({
   reducer: {
     //@ts-ignore
     Auth: AuthReducer,
     friend: friendReducer,
+    chat: chatReducer,
   },
   middleware: [thunkMiddleware],
 });
