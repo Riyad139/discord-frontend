c36d30aa74ac2cb31e11c83a185ee2b9c2e9583d
Riyad139 <riyad.ahmed1886@gmail.com>
2023-04-04 03:25:57 +0600

    side bar is now ready to show rooms

--- Components/SideBar/index.tsx
+++ Components/SideBar/index.tsx
@@ -1,7 +1,13 @@
+import React from "react";
+import { IState } from "@/@types/IState";
 import { Fab } from "@mui/material";
 import { MdGroups } from "react-icons/md";
+import { useSelector } from "react-redux";
 import CreateRoom from "../CreateRoom/CreateRoom";
+import { joinRoomHanclerEmit } from "@/socketIOClient/connectWithSocketIo";
 export default function () {
+  const rooms = useSelector((state: IState) => state.room.roomDetails);
+
   return (
     <div className="bg-dark h-full flex flex-col items-center  ">
       <Fab
@@ -10,6 +16,20 @@ export default function () {
       >
         <MdGroups size={28} color="white" />
       </Fab>
+
+      {rooms?.map((it) => (
+        <Fab
+          key={it._id}
+          onClick={() => joinRoomHanclerEmit({ id: it._id })}
+          className=" bg-mediumBluish bg-blue rounded-2xl z-0 hover:bg-mediumBluish mt-3 mr-2 w-14 h-14 "
+          aria-label="add"
+        >
+          <p className="text-white text-lg">
+            {it.name[0] + "" + it.name[it.name.length - 1]}
+          </p>
+        </Fab>
+      ))}
+
       <CreateRoom />
     </div>
   );
