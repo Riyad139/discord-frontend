c6b97d5c5cab82220063b145c8c9f8fb90733010
Riyad139 <riyad.ahmed1886@gmail.com>
2023-04-07 17:43:19 +0600

    video section is created

--- Components/Room/videoSection.tsx
+++ Components/Room/videoSection.tsx
@@ -1,3 +1,49 @@
+import { IState } from "@/@types/IState";
+import { styled } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import UserWindow from "./UserWindow";
+
+const widthSize = (length: number) => {
+  if (length) return Math.floor(100 / length);
+  return 100;
+};
+const heightSize = (length: number) => {
+  return length / 4;
+};
+
+const VideoDiv = styled("video")({
+  width: "100%",
+  height: "100%",
+});
+
 export default function VideoSection() {
-  return <div className="h-[90%] w-full"></div>;
+  const activeUsers = useSelector((state: IState) => state.room.activeRooms);
+  const [width, setWidth] = useState(100);
+  const thisUser = useSelector((state: IState) => state.Auth.user.email);
+  const localStream = useSelector((state: IState) => state.room.localStram);
+  const videoRef = useRef<HTMLMediaElement>();
+  useEffect(() => {
+    setWidth(widthSize(activeUsers.length));
+    if (videoRef.current) {
+      videoRef.current.srcObject = localStream;
+      videoRef.current.onloadeddata = () => {
+        videoRef.current?.play();
+      };
+    }
+  }, [activeUsers, localStream]);
+
+  return (
+    <div className="h-[90%]  flex flex-wrap w-full">
+      <div className="bg-gray-black w-1/2 h-full">
+        <VideoDiv ref={videoRef} autoPlay muted={true} />
+      </div>
+      {activeUsers.map(
+        (it) =>
+          it.email !== thisUser && (
+            <UserWindow width={activeUsers.length} name={it.username} />
+          )
+      )}
+    </div>
+  );
 }
