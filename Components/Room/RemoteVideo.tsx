006fcfc8c04fab932edecc603db28cf15e20a8d7
Riyad139 <riyad.ahmed1886@gmail.com>
2023-04-09 22:23:11 +0600

    Remote  video component is added

---
+++ Components/Room/RemoteVideo.tsx
@@ -0,0 +1,23 @@
+import { styled } from "@mui/material";
+import { useEffect, useRef } from "react";
+
+const VideoDiv = styled("video")({
+  width: "100%",
+  height: "100%",
+});
+export default function RemoteVideo(stream: any) {
+  const videoRef = useRef<HTMLMediaElement>();
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.srcObject = stream.stream;
+      videoRef.current.onloadeddata = () => {
+        videoRef.current?.play();
+      };
+    }
+  }, [stream]);
+  return (
+    <div className="w-1/2 h-full">
+      <VideoDiv ref={videoRef} autoPlay muted={false} />
+    </div>
+  );
+}
