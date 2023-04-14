5d0207b597785a54f965d6aa1845dc613885d2d5
Riyad139 <riyad.ahmed1886@gmail.com>
2023-04-15 03:48:39 +0600

    new fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-}
+  rewrites: () => {
+    return [
+      {
+        source: "/base/:slug*",
+        destination: "https://discord-server-ecru.vercel.app/api/:slug*",
+        basePath: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
