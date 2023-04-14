6309b60fd823696b2ba32616441b3d82180ec351
Riyad139 <riyad.ahmed1886@gmail.com>
2023-04-14 12:47:25 +0600

    screen shareing is enabled

--- Components/Room/ControllerSection.tsx
+++ Components/Room/ControllerSection.tsx
@@ -8,40 +8,74 @@ import CloseIcon from "@mui/icons-material/Close";
 import { Dispatch, SetStateAction, useState } from "react";
 import { leaveRoomHandlerEmit } from "@/socketIOClient/connectWithSocketIo";
 import { useDispatch, useSelector } from "react-redux";
-import { setMode } from "../app/roomSlice";
-import getLocalStream from "../utils/videoPreview";
+
+import ScreenShareIcon from "@mui/icons-material/ScreenShare";
+import StopScreenShareIcon from "@mui/icons-material/StopScreenShare";
+
 import { IState } from "@/@types/IState";
+import { setScreenShare } from "../app/roomSlice";
+import { switchPeerTrack } from "../utils/PeerConnection";
 
 type IProps = {
   miniMizedhandler: Dispatch<SetStateAction<boolean>>;
   value: boolean;
 };
 
+const constraint = {
+  video: true,
+  audio: false,
+};
+
 export default function ControllerSection({ miniMizedhandler, value }: IProps) {
   const [isVideoEnable, setVideoEnable] = useState(true);
   const [isMicEnable, setMicEnable] = useState(true);
-
+  const screenShareStream = useSelector(
+    (state: IState) => state.room.screenSharingStream
+  );
+  const isScreenShare = useSelector(
+    (state: IState) => state.room.isScreensharingActive
+  );
+  const disPatch = useDispatch();
   const localStream = useSelector((state: IState) => state.room.localStram);
   const handlerVideo = () => {
     localStream.getVideoTracks()[0].enabled = isVideoEnable;
     setVideoEnable((val) => !val);
-
-    console.log(localStream.getVideoTracks());
   };
+  const handlerAudio = () => {
+    setMicEnable((val) => !val);
+    localStream.getAudioTracks()[0].enabled = !isMicEnable;
+  };
+
+  const handlerScreenShare = async () => {
+    let stream = null;
+    if (!isScreenShare) {
+      try {
+        stream = await navigator.mediaDevices.getDisplayMedia(constraint);
+      } catch (error: any) {
+        console.log(error);
+      }
+      if (stream) {
+        disPatch(setScreenShare(stream));
+        switchPeerTrack(stream);
+      }
+    } else {
+      switchPeerTrack(localStream);
+      if (screenShareStream)
+        screenShareStream.getTracks().forEach((it: any) => {
+          it.stop();
+        });
+      disPatch(setScreenShare(null));
+    }
+  };
+
   return (
     <div className="w-full h-[10%] gap-5 flex items-center justify-center bg-mediumBluish">
       <div>
         {isMicEnable && (
-          <MicIcon
-            className="cursor-pointer"
-            onClick={() => setMicEnable((value) => !value)}
-          />
+          <MicIcon className="cursor-pointer" onClick={handlerAudio} />
         )}
         {!isMicEnable && (
-          <MicOffIcon
-            className="cursor-pointer"
-            onClick={() => setMicEnable((value) => !value)}
-          />
+          <MicOffIcon className="cursor-pointer" onClick={handlerAudio} />
         )}
       </div>
       <div>
@@ -52,7 +86,11 @@ export default function ControllerSection({ miniMizedhandler, value }: IProps) {
           <VideocamOffIcon className="cursor-pointer" onClick={handlerVideo} />
         )}
       </div>
-      <div className="">
+      <div>
+        {!isScreenShare && <ScreenShareIcon onClick={handlerScreenShare} />}
+        {isScreenShare && <StopScreenShareIcon onClick={handlerScreenShare} />}
+      </div>
+      <div className="sm:block hidden">
         {value && (
           <div
             className="cursor-pointer"
