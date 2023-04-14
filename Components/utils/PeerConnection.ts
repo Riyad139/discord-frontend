d9846f2092a6bbf77ed73264cab7ed91618aa335
Riyad139 <riyad.ahmed1886@gmail.com>
2023-04-14 12:48:52 +0600

    switching stream is added

--- Components/utils/PeerConnection.ts
+++ Components/utils/PeerConnection.ts
@@ -1,7 +1,9 @@
 import Peer, { Instance } from "simple-peer";
 import store from "../app/store";
 import { sendPeerSignal } from "@/socketIOClient/connectWithSocketIo";
-import { setRemoteStream } from "../app/roomSlice";
+import { setLocalStream, setRemoteStream, setSendData } from "../app/roomSlice";
+import { object } from "yup";
+import { Stream } from "stream";
 
 const getConfig = () => {
   const turnIceServers = null;
@@ -21,7 +23,7 @@ const getConfig = () => {
 let peer: { [key: string]: Instance } = {};
 
 const getPeerSocketPrepare = (connUserSoc: string, isInitatior: boolean) => {
-  const locarStream = store.getState().room.localStram;
+  const locarStream = store.getState().room.sendStreamData;
   if (isInitatior) {
     console.log("preparing connection as initiator");
   } else {
@@ -39,12 +41,13 @@ const getPeerSocketPrepare = (connUserSoc: string, isInitatior: boolean) => {
       signal: data,
       connUserSoc,
     };
-    console.log(signalData);
     sendPeerSignal(signalData);
   });
 
   peer[connUserSoc].on("stream", (stream) => {
     const remoteStream = store.getState().room.remoteStreams;
+    //@ts-ignore
+    stream.connUserSocId = connUserSoc;
     const newRemoteStream = [...remoteStream, stream];
     store.dispatch(setRemoteStream(newRemoteStream));
   });
@@ -53,6 +56,53 @@ const getPeerSocketPrepare = (connUserSoc: string, isInitatior: boolean) => {
 export const handleInCommingSignalingData = (data: any) => {
   const { signal, connUserSoc } = data;
   if (peer[connUserSoc]) peer[connUserSoc].signal(signal);
+};
+
+export const stopAllPeerConnection = () => {
+  Object.entries(peer).forEach((value) => {
+    const connUser = value[0];
+    if (peer[connUser]) {
+      peer[connUser].destroy();
+      delete peer[connUser];
+    }
+  });
+};
+
+export const removeSinglePeerConnection = (conUserSoc: string) => {
+  const remoteStream = store.getState().room.remoteStreams;
+  const updatedSteam = remoteStream.filter(
+    (id) => id.connUserSocId !== conUserSoc
+  );
+
+  peer[conUserSoc].destroy();
+  delete peer[conUserSoc];
+  store.dispatch(setRemoteStream(updatedSteam));
+};
+
+export const switchPeerTrack = (stream: any) => {
+  store.dispatch(setSendData(stream));
+  for (let socketId in peer) {
+    for (let index in peer[socketId].streams[0].getTracks()) {
+      for (let index2 in stream.getTracks()) {
+        console.log(
+          peer[socketId].streams[0].getTracks()[index].kind,
+          stream.getTracks()[index2].kind
+        );
+        if (
+          peer[socketId].streams[0].getTracks()[index].kind ===
+          stream.getTracks()[index2].kind
+        ) {
+          peer[socketId].replaceTrack(
+            peer[socketId].streams[0].getTracks()[index],
+            stream.getTracks()[index2],
+            peer[socketId].streams[0]
+          );
+
+          break;
+        }
+      }
+    }
+  }
 };
 
 export default getPeerSocketPrepare;
