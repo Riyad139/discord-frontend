96d41e220f3ad6455059088888c12ef03f501ff1
Riyad139 <riyad.ahmed1886@gmail.com>
2023-04-14 12:46:39 +0600

    dasboard is responsive

--- Components/DashBoard/DashBoard.tsx
+++ Components/DashBoard/DashBoard.tsx
@@ -2,20 +2,30 @@ import { IState } from "@/@types/IState";
 import { useSelector } from "react-redux";
 import ChatComponent from "../ChatComponent/ChatComponent";
 import Room from "../Room/Room";
+import NavBar from "../NavBar";
+import classNames from "classnames";
 
 export default function DashBoard() {
   const chatInfo = useSelector((state: IState) => state.chat);
   const room = useSelector((state: IState) => state.room);
 
   return (
-    <div className="w-full h-[93.2%] text-gray-200 flex justify-center items-center bg-lightGray">
-      {!chatInfo.ChatType && !room.isUserInRoom && (
-        <p>To start chating - selecet a converstion</p>
+    <div
+      className={classNames(
+        "h-full  w-[85%]",
+        chatInfo.ChatType ? "block" : "hidden sm:block"
       )}
-      {chatInfo.ChatType && !room.isUserInRoom && chatInfo.ChatDetails && (
-        <ChatComponent userId={chatInfo.ChatDetails} />
-      )}
-      {room.isUserInRoom && <Room />}
+    >
+      <NavBar />
+      <div className="text-gray-200 h-[93%]  flex justify-center items-center bg-lightGray">
+        {!chatInfo.ChatType && !room.isUserInRoom && (
+          <p>To start chating - selecet a converstion</p>
+        )}
+        {chatInfo.ChatType && !room.isUserInRoom && chatInfo.ChatDetails && (
+          <ChatComponent userId={chatInfo.ChatDetails} />
+        )}
+        {room.isUserInRoom && <Room />}
+      </div>
     </div>
   );
 }
